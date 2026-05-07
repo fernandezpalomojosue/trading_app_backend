3ce21a3bf6cc332fdc3a72aed87559f8262cf064
fernandezpalomojosue <fernandezpalomojosue21@gmail.com>
2026-05-07 16:05:41 -0400

    Update signal_orchestrator.py

--- app/domain/use_cases/signal_orchestrator.py
+++ app/domain/use_cases/signal_orchestrator.py
@@ -521,7 +521,7 @@ class SignalOrchestrator:
                 action = strategy.dsl_definition.get("action", "buy") if action == "hold" else action
             
             # Generate signal
-            signal = await self.signal_engine_service.calculate_single_signal(
+            signal = self.signal_engine_service.calculate_single_signal(
                 symbol=snapshot.symbol,
                 point=context,
                 prev_point=prev_context,
